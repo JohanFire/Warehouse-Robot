c1a082ad6bb9e892bde5675239fe18762c99e5cb
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-04-04 21:38:10 -0600

    Applying GaussianBlur with kernels

--- computer-vision/finding-lanes/lanes.py
+++ computer-vision/finding-lanes/lanes.py
@@ -5,11 +5,10 @@ import numpy
 
 image = cv2.imread("test_lane_01.jpeg")
 
-# create a copy of original image
 lane_image = numpy.copy(image)
 
-# create grayscale of the color image
 gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+blur = cv2.GaussianBlur(gray, (5, 5), 0) #(image, (kernelSize), deviation)
 
-cv2.imshow('image', gray)
+cv2.imshow('image', blur)
 cv2.waitKey(0)
