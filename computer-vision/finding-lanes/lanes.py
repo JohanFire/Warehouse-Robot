6ac84e8e6885eb8b2d08b8962ffee8692b4758da
EXTERNAL Tristan Garcia Johan (HCM, BGSW/EBM2-SX) <external.Johan.TristanGarcia@mx.bosch.com>
2023-04-05 12:39:18 -0600

    adding Canny to a method

--- computer-vision/finding-lanes/lanes.py
+++ computer-vision/finding-lanes/lanes.py
@@ -3,14 +3,17 @@
 import cv2
 import numpy
 
-image = cv2.imread("test_lane_02.png")
+def canny(lane_image):
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0) #(image, (kernelSize), deviation)
+    canny = cv2.Canny(blur, 50, 150) #(image, lowThreshold, highThreshold)
+    
+    return canny
 
+image = cv2.imread("test_lane_02.png")
 lane_image = numpy.copy(image)
 
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0) #(image, (kernelSize), deviation)
-
-canny = cv2.Canny(blur, 50, 150) #(image, lowThreshold, highThreshold)
+canny = canny(lane_image)
 
 cv2.imshow('image', canny)
 cv2.waitKey(0)
