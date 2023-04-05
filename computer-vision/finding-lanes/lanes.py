2538177da9bf4fad587e00808a4d78b08808b462
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-04-04 21:24:03 -0600

    Creating grayscale of the original image

--- computer-vision/finding-lanes/lanes.py
+++ computer-vision/finding-lanes/lanes.py
@@ -1,7 +1,15 @@
 # lanes = carriles
 
 import cv2
+import numpy
 
 image = cv2.imread("test_lane_01.jpeg")
-cv2.imshow('image', image)
+
+# create a copy of original image
+lane_image = numpy.copy(image)
+
+# create grayscale of the color image
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+cv2.imshow('image', gray)
 cv2.waitKey(0)
