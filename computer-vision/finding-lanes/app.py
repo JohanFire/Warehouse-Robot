98a5582a43b114e1c2c5e2752365cebcce8239de
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-05 18:00:22 -0600

    all adapted to camera

---
+++ computer-vision/finding-lanes/app.py
@@ -0,0 +1,199 @@
+import cv2
+import numpy
+import matplotlib.pyplot as plt
+from icecream import ic
+
+def canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0) #(image, (kernelSize), deviation)
+    canny = cv2.Canny(blur, 50, 150) #(image, lowThreshold, highThreshold)
+    return canny
+
+def region_of_interest(image):
+    """ will return the enclosed region of our field of view and recall that the enclosed region was triangular in shape """
+    # this sizes corresponds to the triangle I drew before in matplotlib
+    # camn array is 2-dimensional: (m, n)
+    height = image.shape[0]
+
+    # set the tringle polygon in an array of polygon
+    polygons = numpy.array([
+        [(200, height), (1100, height), (550, 250)]
+        ])
+
+    # then apply this triangle polygon to a black mask with same dimension
+    mask = numpy.zeros_like(image)
+
+    cv2.fillPoly(mask, polygons, 255) # color of polygon, completely white
+    
+    # will return only the region of interest traced by the polygonal contour of the mask
+    masked_image = cv2.bitwise_and(image, mask) 
+
+    return masked_image
+
+def display_lines(image, lines):
+    """ line_image is an array of zeros with the same shape as the lane images array"""
+    line_image =numpy.zeros_like(image)
+    
+    if lines is not None:
+        for line in lines:
+            ic(line)
+            """ each line is a 2D array containing line coordinates [[x1, y1, x2, y2]] 
+            now will reshape every line into a 1D array [x1, y1, x2, y2]
+            """
+            # line = line.reshape(4)
+            x1, y1, x2, y2 = line.reshape(4)
+
+            # draw line
+            """ args(line_image, (first point of the line), (second point)), color, thickness """
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0 ,0), 10)
+    
+    return line_image
+
+def make_coordinates(image, line_parameters):
+    """ return value that denotes X and Y coordinates of the line. \n
+    ``y = mx + b`` \n
+    where x = (y-b) / m \n
+    """
+    slope, intercept = line_parameters
+    
+    ic(image.shape)
+    """
+    image.shape: (704, 1279, 3)
+    image.shape: (height, width, numberOfChannels)
+    """ 
+
+    y1 = image.shape[0]
+    y2 = int(y1 * (3/5))
+
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+
+    return numpy.array([x1, y1, x2, y2])
+
+def average_slop_intercept(image, lines):
+    """ calculate the slope(pendiente)
+    ---
+    ``y = mx + b`` \n
+    where m = (y2-y1) / (x2-x1) \n
+    b = y - mx
+    """
+    left_fit = []
+    right_fit = []
+
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        
+        """ polyfit will a first degree polynomial which would simply be a linear function of Y is equal to MX plus B,
+        and its going to fit this polynomial to our X&Y points and return a vector of coefficients which describe the slope and Y intercept """
+        parameters = numpy.polyfit((x1, x2), (y1, y2), 1)
+        ic(parameters)
+
+        slope = parameters[0]
+        intercept = parameters[1]
+
+        """ determine if the slope of the line corresponds to the left or right line """
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+    
+    ic(left_fit)
+    ic(right_fit)
+
+    left_fit_average = numpy.average(left_fit, axis=0)
+    right_fit_average = numpy.average(right_fit, axis=0)
+
+    ic(left_fit_average, right_fit_average)
+
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+
+    return numpy.array([left_line, right_line])
+
+def main():
+    cap = cv2.VideoCapture(0)
+    # OpenCV uses 640 x 480 by default, higher makes it slow
+    # capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    # capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    while True:
+        _, frame = cap.read()
+
+        canny_image = canny(frame)
+        cropped_image = region_of_interest(canny_image)
+        lines = cv2.HoughLinesP(
+            cropped_image, 
+            2, 
+            numpy.pi/180, 100, 
+            numpy.array([]),
+            minLineLength=40,
+            maxLineGap=5
+        )
+
+        try:
+            averaged_lines = average_slop_intercept(frame, lines)
+        except Exception as e:
+            print(e)
+        line_image = display_lines(frame, lines)
+        combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1) 
+
+        """ draw the middle line between left & right lane """
+        middle_line_coordinates = [averaged_lines[1][0] - averaged_lines[0][0], int((averaged_lines[1][2] + averaged_lines[0][2])/2)]
+        ic(middle_line_coordinates)
+
+        average_line_image = display_lines(frame, averaged_lines)
+        average_combo_image = cv2.addWeighted(frame, 0.8, average_line_image, 1, 1)
+
+        reference_line = cv2.line(average_combo_image, (640, 560), (640, 720), (0, 255 ,0), 3)
+        middle_line = cv2.line(average_combo_image, (middle_line_coordinates[1], 432), (middle_line_coordinates[0], 720), (255, 0 ,0), 3)
+
+        # half of the middle_line, yellow color
+        half_line = cv2.line(average_combo_image, (620, 560), (660, 560), (0, 255 ,222), 3)
+        # half_line_2 = cv2.line(average_combo_image, (int(middle_line_coordinates[1]), 560), (middle_line_coordinates[0], 560), (0, 0 ,255), 2)
+
+        middle_line = cv2.line(average_combo_image, (middle_line_coordinates[1], 432), (middle_line_coordinates[0], 720), (100, 0 ,255), 1)
+
+        middle_dot_coordinates = (int((middle_line_coordinates[0] + middle_line_coordinates[1])/2))
+        ic(middle_dot_coordinates)
+        middle_dot = cv2.circle(average_combo_image, (middle_dot_coordinates, 560), 5, (100, 0 ,255), -1) 
+        
+        if middle_dot_coordinates > 640:
+            direction_text = cv2.putText(average_combo_image, '<- Left', (middle_dot_coordinates+20, 540), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 0 ,255), 2, cv2.LINE_AA
+            )
+        elif middle_dot_coordinates < 640:
+            direction_text = cv2.putText(average_combo_image, 'Right ->', (middle_dot_coordinates-180, 540), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 0 ,255), 2, cv2.LINE_AA
+            )
+        else:
+            direction_text = cv2.putText(average_combo_image, 'Good', (middle_dot_coordinates-40, 500), 
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (100, 0 ,255), 2, cv2.LINE_AA
+            )
+
+        test = cv2.circle(average_combo_image, (middle_dot_coordinates, 560), 5, (100, 0 ,255), -1) 
+
+
+        
+        """ Resized images, for show """
+        lane_imageResized = cv2.resize(frame,(720,480))
+        canny_imageResized = cv2.resize(canny_image,(720,480))
+        cropped_imageResized = cv2.resize(cropped_image,(720,480))
+        line_imageResized = cv2.resize(line_image,(720,480)) 
+        averaged_line_imageResized = cv2.resize(average_line_image,(720,480)) 
+        # combo_imageResized = cv2.resize(combo_image, (720, 480))
+
+        cv2.imshow('lane_image', lane_imageResized)
+        cv2.imshow('canny', canny_imageResized)
+        cv2.imshow('region_of_interest', cropped_imageResized)
+        cv2.imshow('lines', line_imageResized)
+        cv2.imshow('averaged_line', averaged_line_imageResized)
+
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
