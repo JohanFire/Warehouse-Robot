4204be42f0f9625d31942ca7682df3f6318b8724
Johan Tristán <68982148+JohanFire@users.noreply.github.com>
2023-12-06 13:24:50 -0600

    hidden page web

--- app/control.py
+++ app/control.py
@@ -8,3 +8,17 @@ bp = Blueprint('control', __name__, url_prefix='/')
 @bp.route('/', methods=['GET'])
 def index():
     return render_template('control/index.html')
+
+@bp.route('/hidden', methods=['GET'])
+def hidden():
+    return render_template('hidden.html')
+
+
+
+def funcion_uno():
+    # Lógica para la función uno
+    return "Función Uno ejecutada"
+
+def funcion_dos():
+    # Lógica para la función dos
+    return "Función Dos ejecutada"
